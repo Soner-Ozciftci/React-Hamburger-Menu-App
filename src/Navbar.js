3008780d986f6ae8de8aff2e2e8bb36dd4032018
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-12-15 23:27:05 +0100

    menu class

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,27 @@
-import React from 'react'
-import {useState} from 'react';
+import React from "react";
+import { useState } from "react";
 import "./Navbar.css";
-
 
 export default function Navbar() {
 
-const [burger_class,setBurgerClass]=useState("burger-bar unclicked")
-const [menu_class,setMenuClass]=useState("menu hidden")
-const [isMenuClicked,setIsMenuClicked]=useState(false)
+  const [burger_class,setBurgerClass]=useState("burger-bar unclicked");
+  const [menu_class,setMenuClass]=useState("menu hidden");
+  const [isMenuClicked,setIsMenuClicked]=useState(false);
 
   return (
     <div style={{width:"100%",height:"100vh"}}>
-
+      <nav>
+        <div className="burger-menu">
+          <div className={burger_class}></div>
+          <div className={burger_class}></div>
+          <div className={burger_class}></div>
+        </div>
+      </nav>
+      <div className={menu_class}>
+        <h3>Home</h3>
+        <h3>About</h3>
+        <h3>Contact</h3>
+      </div>
     </div>
-  )
+  );
 }
