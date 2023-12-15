f16bd4d856b91cd032f99c37ee9b54c183130b09
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-12-15 23:21:15 +0100

    css dizayn

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,14 @@ import "./Navbar.css";
 
 
 export default function Navbar() {
+
+const [burger_class,setBurgerClass]=useState("burger-bar unclicked")
+const [menu_class,setMenuClass]=useState("menu hidden")
+const [isMenuClicked,setIsMenuClicked]=useState(false)
+
   return (
-    <div>Navbar</div>
+    <div style={{width:"100%",height:"100vh"}}>
+
+    </div>
   )
 }
